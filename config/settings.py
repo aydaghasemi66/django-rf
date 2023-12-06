260e9f652ec85094ee4f266c0afb13d5a2b95f81
Ayda <136740721+aydaghasemi66@users.noreply.github.com>
2023-12-06 08:37:15 -0800

    api folders

--- config/settings.py
+++ config/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'blogs',
     'portfolio',
     'product',
+    'rest_framework',
 
 ]
 
@@ -135,3 +136,15 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+AUTH_USER_MODEL = 'accounts.CustomeUser'
+
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
+}
